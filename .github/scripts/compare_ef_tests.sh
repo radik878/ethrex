cb0f0cd6314b647d52fdee52c072f254461ce7d2
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-12 14:40:44 -0300

    ci(levm): remove additional ef-test run in benchmarks job (#1925)
    **Motivation**
    
    With #1894, there's already a table showing how many ef-tests pass.
    Thus, this additional ef-test run is a bit redundant.
    Furthermore, the `compare_results.sh` also receives some improvements.
    
    
    **Description**
    This commit:
    - Removes the `ef-test` job from the `ci_bench_levm_in_pr.yaml` file
    - Adds the completion percentage in the comparison chart
    - Adds the amount of tests that now pass (So it's the up/down emoji +
    the difference)
    
    ---------
    
    Co-authored-by: Federico Borello <156438142+fborello-lambda@users.noreply.github.com>

--- .github/scripts/compare_ef_tests.sh
+++ .github/scripts/compare_ef_tests.sh
@@ -20,10 +20,19 @@ do
    name_main=$(echo "$i" | awk -F " " '{print $1}')
    result_main=$(echo "$i" | awk -F " " '{print $2}')
    result_main=${result_main%(*}
+   percentage_main=$(echo "$i" | awk -F " " '{print $3}')
 
    name_pr=$(echo "${pr_results[num]}" | awk -F " " '{print $1}')
    result_pr=$(echo "${pr_results[num]}" | awk -F " " '{print $2}')
    result_pr=${result_pr%(*}
+   percentage_pr=$(echo "${pr_results[num]}" | awk -F " " '{print $3}')
+
+   passing_pr=${result_pr%/*}
+   passing_main=${result_main%/*}
+   difference=$(echo "$passing_pr - $passing_main" | bc)
+   if [ $difference == "0" ]; then
+       difference=""
+   fi
 
    emoji=""
    if (( $(echo "$result_main > $result_pr" |bc -l) )); then
@@ -34,7 +43,8 @@ do
        emoji="➖️"
    fi
 
-   echo "|$name_main|$result_main|$result_pr| $emoji |"
+
+   echo "|$name_main|$result_main $percentage_main |$result_pr $percentage_pr| $emoji $difference |"
 
    num=$((num + 1))
 
