d13146712935a53f24f056e947cb3b3628bfbe6b
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-13 14:19:58 -0300

    fix(levm): add gas cost of pre tangerine `CALL*` family (#1932)
    **Motivation**
    
    Before EIP-150, the gas cost of CALL, DELEGATECALL, CALLCODE opcode was
    40 (see: https://eips.ethereum.org/EIPS/eip-150).
    
    This PR incorporates that 40 gas cost for VM instances running before or
    equal to the DaoFork
    
    **Description**
    
    All these functions used the `access_address_cost` function to calculate
    their gas cost. Replicate the if statement in that function in the case
    the fork is equal to or lower than the Dao Fork.

--- crates/vm/levm/src/gas_cost.rs
+++ crates/vm/levm/src/gas_cost.rs
@@ -154,6 +154,7 @@ pub const STATICCALL_COLD_DYNAMIC: u64 = DEFAULT_COLD_DYNAMIC;
 pub const STATICCALL_WARM_DYNAMIC: u64 = DEFAULT_WARM_DYNAMIC;
 
 // Costs in gas for call opcodes
+pub const ADDRESS_COST_PRE_DAO: u64 = 40;
 pub const ADDRESS_COST_PRE_BERLIN: u64 = 700;
 pub const WARM_ADDRESS_ACCESS_COST: u64 = 100;
 pub const COLD_ADDRESS_ACCESS_COST: u64 = 2600;
@@ -635,8 +636,10 @@ fn address_access_cost(
     warm_dynamic_cost: u64,
     fork: Fork,
 ) -> Result<u64, VMError> {
-    // [EIP-2929](https://eips.ethereum.org/EIPS/eip-2929)
-    if fork <= Fork::Berlin {
+    if fork <= Fork::DaoFork {
+        Ok(ADDRESS_COST_PRE_DAO)
+    } else if fork > Fork::DaoFork && fork <= Fork::Berlin {
+        // [EIP-2929](https://eips.ethereum.org/EIPS/eip-2929)
         Ok(ADDRESS_COST_PRE_BERLIN)
     } else {
         let static_gas = static_cost;
