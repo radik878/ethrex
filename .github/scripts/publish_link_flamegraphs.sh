e41c954fd3c8c76ebf88169b57e7a9c445b502e3
Jeremías Salomón <48994069+JereSalo@users.noreply.github.com>
2025-02-28 13:41:38 -0300

    fix(levm): fix flamegraph comparison and change some things (#2107)
    **Summary**
    - Fix related issues with flamegraph and the webpage
    
    **Description**
    
    - Updates rust version to 1.85 because we had an error that a crate
    required rust 2024 to work.
    - Then there was some trouble with `derive_more`, which is a crate used
    by other crates that we use. We didn't have anything in our `Cargo.toml`
    files regarding that crate but to fix the problem I added it enabling
    all it's features for the 1.0.0 version.
    - I changed .svg and .txt links so that we go from
    `/flamegraph_ethrex.svg/flamegraph_ethrex.svg` to just
    `/flamegraph_ethrex.svg`
    - I fixed REVM's download link because it didn't work.
    - I added the choice to open flamegraph in new tab instead of
    downloading it
    
    
    You can see the results of these changes at
    https://lambdaclass.github.io/ethrex/

--- .github/scripts/publish_link_flamegraphs.sh
+++ .github/scripts/publish_link_flamegraphs.sh
@@ -15,8 +15,8 @@ curl -XPOST -H "Content-type: application/json" -d '{
       "text": {
         "type": "mrkdwn",
         "text": "Flamegraphs are available at *<https://lambdaclass.github.io/ethrex/|https://lambdaclass.github.io/ethrex/>*\n
-        • *<https://lambdaclass.github.io/ethrex/flamegraph_ethrex.svg/flamegraph_ethrex.svg|Ethrex>*\n
-        • *<https://lambdaclass.github.io/ethrex/flamegraph_reth.svg/flamegraph_reth.svg|Reth>*\n"
+        • *<https://lambdaclass.github.io/ethrex/flamegraph_ethrex.svg|Ethrex>*\n
+        • *<https://lambdaclass.github.io/ethrex/flamegraph_reth.svg|Reth>*\n"
       }
     },
   ],
