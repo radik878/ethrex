effc5341e38646b8b07fa9f76bb067533d375846
Tomás Paradelo <112426153+tomip01@users.noreply.github.com>
2025-03-10 13:52:15 -0300

    test(levm): ensure behavior pairing check (#2160)
    **Motivation**
    
    This PR is for ensuring that the precompile bls_pairing_check works as
    expected.
    If any of the points in the pairs is a infinity point, that specific
    pair should be skipped.
    
    **Description**
    
    We add a unit test that test this behavior. If a pair with a infinity
    point is added, the result must not change.

--- crates/vm/levm/tests/tests.rs
+++ crates/vm/levm/tests/tests.rs
@@ -20,8 +20,8 @@ use ethrex_levm::{
     memory,
     operations::Operation,
     precompiles::{
-        blake2f, bls12_g1msm, ecadd, ecmul, ecpairing, ecrecover, identity, modexp, ripemd_160,
-        sha2_256,
+        blake2f, bls12_g1msm, bls12_pairing_check, ecadd, ecmul, ecpairing, ecrecover, identity,
+        modexp, ripemd_160, sha2_256,
     },
     testing::{new_vm_with_ops, new_vm_with_ops_addr_bal_db, new_vm_with_ops_db, ops_to_bytecode},
     utils::{calculate_create_address, word_to_address},
@@ -4694,4 +4694,35 @@ fn g1_mul() {
 
     assert_eq!(result, Bytes::copy_from_slice(&expected_result));
     assert_eq!(consumed_gas, 12000); // Verify gas consumption
+}
+
+#[test]
+fn pairing_infinity() {
+    let zero = Bytes::copy_from_slice(&[0_u8; 32]);
+
+    // This is a calldata that pairing check returns 0
+    // This is from https://eips.ethereum.org/assets/eip-2537/pairing_check_bls.json
+    // test "bls_pairing_non-degeneracy"
+    let mut calldata = hex::decode("0000000000000000000000000000000017f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb0000000000000000000000000000000008b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e100000000000000000000000000000000024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb80000000000000000000000000000000013e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e000000000000000000000000000000000ce5d527727d6e118cc9cdc6da2e351aadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801000000000000000000000000000000000606c4a02ea734cc32acd2b02bc28b99cb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be").unwrap();
+    let calldata_bytes = Bytes::from(calldata.clone());
+    let mut consumed_gas = 0;
+
+    let result = bls12_pairing_check(&calldata_bytes, 10000000, &mut consumed_gas);
+    assert_eq!(result.unwrap(), zero);
+
+    // Now we add a pair were one point is infinity, the result must not change
+
+    // This represent a G1 infinity point
+    calldata.extend_from_slice(&[0u8; 128]);
+
+    // This a valid calldata from the test "bls_pairing_e(G1,-G2)=e(-G1,G2)" of the same link
+    let valid_calldata = hex::decode("0000000000000000000000000000000017f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb0000000000000000000000000000000008b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e100000000000000000000000000000000024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb80000000000000000000000000000000013e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e000000000000000000000000000000000ce5d527727d6e118cc9cdc6da2e351aadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801000000000000000000000000000000000606c4a02ea734cc32acd2b02bc28b99cb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be0000000000000000000000000000000017f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb0000000000000000000000000000000008b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e100000000000000000000000000000000024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb80000000000000000000000000000000013e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e000000000000000000000000000000000d1b3cc2c7027888be51d9ef691d77bcb679afda66c73f17f9ee3837a55024f78c71363275a75d75d86bab79f74782aa0000000000000000000000000000000013fa4d4a0ad8b1ce186ed5061789213d993923066dddaf1040bc3ff59f825c78df74f2d75467e25e0f55f8a00fa030ed").unwrap();
+    // We add only the first G2 point of the calldata
+    calldata.extend_from_slice(valid_calldata.get(128..384).unwrap());
+
+    let calldata_bytes = Bytes::from(calldata.clone());
+
+    let result = bls12_pairing_check(&calldata_bytes, 10000000, &mut consumed_gas);
+
+    assert_eq!(result.unwrap(), zero);
 }
