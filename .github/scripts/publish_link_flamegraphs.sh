892d5adb946de77d2be9586e44225ab702622e99
Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>
2025-03-14 13:08:02 -0300

    fix(core): fix slack flamegraphs link (#2228)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes #issue_number

--- .github/scripts/publish_link_flamegraphs.sh
+++ .github/scripts/publish_link_flamegraphs.sh
@@ -14,7 +14,7 @@ curl -XPOST -H "Content-type: application/json" -d '{
       "type": "section",
       "text": {
         "type": "mrkdwn",
-        "text": "Flamegraphs are available at *<https://lambdaclass.github.io/ethrex/|https://lambdaclass.github.io/ethrex/flamegraphs>*\n
+        "text": "Flamegraphs are available at *<https://lambdaclass.github.io/ethrex/flamegraphs|https://lambdaclass.github.io/ethrex/flamegraphs>*\n
         • *<https://lambdaclass.github.io/ethrex/flamegraphs/flamegraph_ethrex.svg|Ethrex>*\n
         • *<https://lambdaclass.github.io/ethrex/flamegraphs/flamegraph_reth.svg|Reth>*\n"
       }
