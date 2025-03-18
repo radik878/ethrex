87fc76a74cda059d2bcf25172d26d69bd9fcd8e8
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2025-03-18 16:24:26 +0000

    chore(core): improve double genesis block run error (#2252)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    It is not obvious how to mitigate this error for users who find
    themselves having this error. Having a better better error comment might
    help with this.

--- crates/storage/store.rs
+++ crates/storage/store.rs
@@ -445,7 +445,7 @@ impl Store {
                 info!("Received genesis file matching a previously stored one, nothing to do");
                 return Ok(());
             } else {
-                panic!("tried to run genesis twice with different blocks");
+                panic!("Tried to run genesis twice with different blocks. Try again after clearing the database. If you're running ethrex as an Ethereum client, run cargo run --release --bin ethrex -- removedb; if you're running ethrex as an L2 run make rm-db-l1 rm-db-l2");
             }
         }
         // Store genesis accounts
