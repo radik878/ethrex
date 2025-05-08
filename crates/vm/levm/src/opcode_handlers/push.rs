9ace51f37360186a4d4ad31a7ab32865071e2fe7
Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>
2025-05-08 15:30:25 -0300

    perf(levm): remove unnecessary double copying in op_push (#2702)
    **Motivation**
    
    Removes a call to the `bytes_to_word` function (and the function
    itself), as it was unnecessary and implied copying the same slice twice.
    
    I noticed while syncing in Holesky that (somewhat expectedly) a lot of
    time was spent on `op_push`, and while looking deeper realized that
    there was unnecessary work being done.
    
    Flamegraph on `op_push` on main:
    <img width="1504" alt="Screenshot 2025-05-08 at 11 32 37"
    src="https://github.com/user-attachments/assets/e990fa05-9a7b-4ba5-8a5b-7b177eeb25d4"
    />
    
    Flamegraph on `op_push` on this branch:
    <img width="1502" alt="Screenshot 2025-05-08 at 11 33 01"
    src="https://github.com/user-attachments/assets/ed0fc3d8-8c6b-4a11-9ca2-16d8f7afd1e1"
    />
    
    My impression is that there's still work to be done on `op_push`
    however.
    
    The bench comparison against `revm` in the comments shows around a ~5-6%
    improvement.
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes #issue_number

--- crates/vm/levm/src/opcode_handlers/push.rs
+++ crates/vm/levm/src/opcode_handlers/push.rs
@@ -1,6 +1,5 @@
 use crate::{
     call_frame::CallFrame,
-    constants::WORD_SIZE,
     errors::{InternalError, OpcodeResult, VMError},
     gas_cost,
     vm::VM,
@@ -17,11 +16,10 @@ impl<'a> VM<'a> {
         current_call_frame.increase_consumed_gas(gas_cost::PUSHN)?;
 
         let read_n_bytes = read_bytcode_slice(current_call_frame, n_bytes)?;
-        let value_to_push = bytes_to_word(read_n_bytes, n_bytes)?;
 
         current_call_frame
             .stack
-            .push(U256::from_big_endian(value_to_push.as_slice()))?;
+            .push(U256::from_big_endian(read_n_bytes))?;
 
         // The n_bytes that you push to the stack + 1 for the next instruction
         let increment_pc_by = n_bytes.wrapping_add(1);
@@ -63,22 +61,4 @@ fn read_bytcode_slice(current_call_frame: &CallFrame, n_bytes: usize) -> Result<
         .bytecode
         .get(pc_offset..pc_offset.checked_add(n_bytes).ok_or(VMError::OutOfBounds)?)
         .unwrap_or_default())
-}
-
-fn bytes_to_word(read_n_bytes: &[u8], n_bytes: usize) -> Result<[u8; WORD_SIZE], VMError> {
-    let mut value_to_push = [0u8; WORD_SIZE];
-    let start_index = WORD_SIZE.checked_sub(n_bytes).ok_or(VMError::Internal(
-        InternalError::ArithmeticOperationUnderflow,
-    ))?;
-
-    for (i, byte) in read_n_bytes.iter().enumerate() {
-        let index = start_index.checked_add(i).ok_or(VMError::Internal(
-            InternalError::ArithmeticOperationOverflow,
-        ))?;
-        if let Some(data_byte) = value_to_push.get_mut(index) {
-            *data_byte = *byte;
-        }
-    }
-
-    Ok(value_to_push)
 }
