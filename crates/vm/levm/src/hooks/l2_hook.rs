f5cac07c5b8cc69d6d798c958c091004e9cbd72b
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-07-03 12:35:17 -0300

    refactor(l2): simplify l2 hook (#3392)
    **Motivation**
    
    We want to reuse the default hook as much as possible. Since only
    privileged transactions need special handling, the l2 hook should focus
    on them.
    
    **Description**
    
    - Gas is not paid by privileged transactions
        - Therefore gas prices are irrelevant
    - We don't want to overflow block limits. This is the only case when a
    privileged transaction can error out
    - Privileged transactions can't _fail_ as that would cause them to not
    be included, stalling deposits
        - Instead, we make the transaction revert
    
    Closes #3343
    
    ---------
    
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- crates/vm/levm/src/hooks/l2_hook.rs
+++ crates/vm/levm/src/hooks/l2_hook.rs
@@ -1,11 +1,11 @@
 use crate::{
-    errors::{ContextResult, InternalError, TxValidationError},
-    hooks::{default_hook, hook::Hook},
+    errors::{ContextResult, InternalError},
+    hooks::{DefaultHook, default_hook, hook::Hook},
     opcodes::Opcode,
     vm::VM,
 };
 
-use ethrex_common::{Address, H160, U256, types::Fork};
+use ethrex_common::{Address, H160, U256};
 
 pub const COMMON_BRIDGE_L2_ADDRESS: Address = H160([
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -16,26 +16,24 @@ pub struct L2Hook {}
 
 impl Hook for L2Hook {
     fn prepare_execution(&mut self, vm: &mut VM<'_>) -> Result<(), crate::errors::VMError> {
-        let sender_address = vm.env.origin;
-        let (sender_balance, sender_nonce) = {
-            let sender_account = vm.db.get_account(sender_address)?;
-            (sender_account.info.balance, sender_account.info.nonce)
-        };
+        if !vm.env.is_privileged {
+            return DefaultHook.prepare_execution(vm);
+        }
 
-        let mut privileged_had_insufficient_balance = false;
+        let sender_address = vm.env.origin;
+        let sender_balance = vm.db.get_account(sender_address)?.info.balance;
+
+        let mut tx_should_fail = false;
 
         // The bridge is allowed to mint ETH.
         // This is done by not decreasing it's balance when it's the source of a transfer.
         // For other privileged transactions, insufficient balance can't cause an error
         // since they must always be accepted, and an error would mark them as invalid
         // Instead, we make them revert by inserting a revert2
-        if vm.env.is_privileged && sender_address != COMMON_BRIDGE_L2_ADDRESS {
+        if sender_address != COMMON_BRIDGE_L2_ADDRESS {
             let value = vm.current_call_frame()?.msg_value;
             if value > sender_balance {
-                privileged_had_insufficient_balance = true;
-                vm.current_call_frame_mut()?.msg_value = U256::zero();
-                vm.current_call_frame_mut()?
-                    .set_code(vec![Opcode::INVALID.into()].into())?;
+                tx_should_fail = true;
             } else {
                 // This should never fail, since we just checked the balance is enough.
                 vm.decrease_account_balance(sender_address, value)
@@ -47,83 +45,52 @@ impl Hook for L2Hook {
             }
         }
 
-        if vm.env.config.fork >= Fork::Prague {
-            default_hook::validate_min_gas_limit(vm)?;
-        }
+        // if fork > prague: default_hook::validate_min_gas_limit
+        // NOT CHECKED: the l1 makes spamming privileged transactions not economical
 
-        if !vm.env.is_privileged {
-            // (1) GASLIMIT_PRICE_PRODUCT_OVERFLOW
-            let gaslimit_price_product = vm
-                .env
-                .gas_price
-                .checked_mul(vm.env.gas_limit.into())
-                .ok_or(TxValidationError::GasLimitPriceProductOverflow)?;
-
-            default_hook::validate_sender_balance(vm, sender_balance)?;
-
-            // (3) INSUFFICIENT_ACCOUNT_FUNDS
-            default_hook::deduct_caller(vm, gaslimit_price_product, sender_address)?;
-
-            // (7) NONCE_IS_MAX
-            vm.increment_account_nonce(sender_address)
-                .map_err(|_| TxValidationError::NonceIsMax)?;
-
-            // check for nonce mismatch
-            if sender_nonce != vm.env.tx_nonce {
-                return Err(TxValidationError::NonceMismatch {
-                    expected: sender_nonce,
-                    actual: vm.env.tx_nonce,
-                }
-                .into());
-            }
-
-            // (9) SENDER_NOT_EOA
-            default_hook::validate_sender(vm.db.get_account(sender_address)?)?;
-        }
+        // (1) GASLIMIT_PRICE_PRODUCT_OVERFLOW
+        // NOT CHECKED: privileged transactions do not pay for gas
 
         // (2) INSUFFICIENT_MAX_FEE_PER_BLOB_GAS
-        if let Some(tx_max_fee_per_blob_gas) = vm.env.tx_max_fee_per_blob_gas {
-            default_hook::validate_max_fee_per_blob_gas(vm, tx_max_fee_per_blob_gas)?;
-        }
+        // NOT CHECKED: the blob price does not matter, privileged transactions do not support blobs
 
         // (4) INSUFFICIENT_MAX_FEE_PER_GAS
-        default_hook::validate_sufficient_max_fee_per_gas(vm)?;
+        // NOT CHECKED: privileged transactions do not pay for gas, the gas price is irrelevant
 
         // (5) INITCODE_SIZE_EXCEEDED
-        if vm.is_create()? {
-            default_hook::validate_init_code_size(vm)?;
-        }
+        // NOT CHECKED: privileged transactions can't be of "create" type
 
         // (6) INTRINSIC_GAS_TOO_LOW
-        vm.add_intrinsic_gas()?;
-
-        // (8) PRIORITY_GREATER_THAN_MAX_FEE_PER_GAS
-        if let (Some(tx_max_priority_fee), Some(tx_max_fee_per_gas)) = (
-            vm.env.tx_max_priority_fee_per_gas,
-            vm.env.tx_max_fee_per_gas,
-        ) {
-            if tx_max_priority_fee > tx_max_fee_per_gas {
-                return Err(TxValidationError::PriorityGreaterThanMaxFeePerGas.into());
-            }
+        // CHANGED: the gas should be charged, but the transaction shouldn't error
+        if vm.add_intrinsic_gas().is_err() {
+            tx_should_fail = true;
         }
 
+        // (7) NONCE_IS_MAX
+        // NOT CHECKED: privileged transactions don't use the account nonce
+
+        // (8) PRIORITY_GREATER_THAN_MAX_FEE_PER_GAS
+        // NOT CHECKED: privileged transactions do not pay for gas, the gas price is irrelevant
+
+        // (9) SENDER_NOT_EOA
+        // NOT CHECKED: contracts can also send privileged transactions
+
         // (10) GAS_ALLOWANCE_EXCEEDED
+        // CHECKED: we don't want to exceed block limits
         default_hook::validate_gas_allowance(vm)?;
 
         // Transaction is type 3 if tx_max_fee_per_blob_gas is Some
-        if vm.env.tx_max_fee_per_blob_gas.is_some() {
-            default_hook::validate_4844_tx(vm)?;
-        }
+        // NOT CHECKED: privileged transactions are not type 3
 
-        // [EIP-7702]: https://eips.ethereum.org/EIPS/eip-7702
         // Transaction is type 4 if authorization_list is Some
-        if vm.tx.authorization_list().is_some() {
-            default_hook::validate_type_4_tx(vm)?;
-        }
+        // NOT CHECKED: privileged transactions are not type 4
 
-        if privileged_had_insufficient_balance {
-            // If the transaction is privileged and had insufficient balance, we already set the bytecode
-            // to INVALID and we need to return here to avoid setting the bytecode again.
+        if tx_should_fail {
+            // If the transaction failed some validation, but it must still be included
+            // To prevent it from taking effect, we force it to revert
+            vm.current_call_frame_mut()?.msg_value = U256::zero();
+            vm.current_call_frame_mut()?
+                .set_code(vec![Opcode::INVALID.into()].into())?;
             return Ok(());
         }
 
@@ -139,20 +106,16 @@ impl Hook for L2Hook {
         vm: &mut VM<'_>,
         ctx_result: &mut ContextResult,
     ) -> Result<(), crate::errors::VMError> {
-        if !ctx_result.is_success() {
+        if !vm.env.is_privileged {
+            return DefaultHook.finalize_execution(vm, ctx_result);
+        }
+
+        if !ctx_result.is_success() && vm.env.origin != COMMON_BRIDGE_L2_ADDRESS {
             default_hook::undo_value_transfer(vm)?;
         }
 
-        // 2. Return unused gas + gas refunds to the sender.
-
-        if !vm.env.is_privileged {
-            let gas_refunded = default_hook::compute_gas_refunded(vm, ctx_result)?;
-            let actual_gas_used =
-                default_hook::compute_actual_gas_used(vm, gas_refunded, ctx_result.gas_used)?;
-            default_hook::refund_sender(vm, ctx_result, gas_refunded, actual_gas_used)?;
-            default_hook::pay_coinbase(vm, actual_gas_used)?;
-        }
-
+        // Even if privileged transactions themselves can't create
+        // They can call contracts that use CREATE/CREATE2
         default_hook::delete_self_destruct_accounts(vm)?;
 
         Ok(())
