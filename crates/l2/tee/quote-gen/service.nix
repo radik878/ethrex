398a10878145cbb6e1657e2360dc24a0518fbee6
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-07-10 10:50:44 -0300

    ci(l2): use correct toolchain in nix build (#3507)
    **Motivation**
    
    Currently the rust version is the one in nixpkgs, which might not follow
    our upgrades.
    
    **Description**
    
    Change the build to rely on the toolchain file on the project root.
    
    ---------
    
    Co-authored-by: Avila Gastón <72628438+avilagaston9@users.noreply.github.com>
    Co-authored-by: avilagaston9 <gaston.avila@lambdaclass.com>

--- crates/l2/tee/quote-gen/service.nix
+++ crates/l2/tee/quote-gen/service.nix
@@ -1,5 +1,19 @@
 let
   pkgs = import <nixpkgs> { };
+  fenix = pkgs.callPackage (pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "fenix";
+    rev = "95606d64662a730da5d3031ed798dd6315d35f33";
+    hash = "sha256-aP4korVsN5Yy+PB9zjjm8Qbo3a69/m8vlFXS5mdVXtk=";
+  }) { };
+  toolchain = fenix.fromToolchainFile {
+      file = ../../../../rust-toolchain.toml;
+      sha256 = "sha256-KUm16pHj+cRedf8vxs/Hd2YWxpOrWZ7UOrwhILdSJBU=";
+  };
+  rustPlatform = pkgs.makeRustPlatform {
+    cargo = toolchain;
+    rustc = toolchain;
+  };
   gitignoreSrc = pkgs.fetchFromGitHub { 
     owner = "hercules-ci";
     repo = "gitignore.nix";
@@ -9,14 +23,14 @@ let
   inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
 in
 let
-  quoteGen = pkgs.rustPlatform.buildRustPackage rec {
+  quoteGen = rustPlatform.buildRustPackage rec {
     pname = "quote-gen";
     version = "0.1";
 
     src = gitignoreSource ./../../../../.;
     sourceRoot = "${src.name}/crates/l2/tee/quote-gen";
 
-    cargoDeps = pkgs.rustPlatform.importCargoLock {
+    cargoDeps = rustPlatform.importCargoLock {
       lockFile = ./Cargo.lock;
       outputHashes = {
         "bls12_381-0.8.0" = "sha256-8/pXRA7hVAPeMKCZ+PRPfQfxqstw5Ob4MJNp85pv5WQ=";
@@ -29,7 +43,7 @@ let
     buildInputs = [ pkgs.openssl ];
     nativeBuildInputs = [
       pkgs.pkg-config
-      pkgs.rustPlatform.cargoSetupHook
+      rustPlatform.cargoSetupHook
     ];
     env.OPENSSL_NO_VENDOR = 1;
   };
