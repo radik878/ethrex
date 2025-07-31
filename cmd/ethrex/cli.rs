dcf04b3ca9c18a11d62b68073ebe5f05ccaac8f5
fmoletta <99273364+fmoletta@users.noreply.github.com>
2025-07-31 10:28:37 -0300

    feat(l1): add specific error message when import is used with the wrong network (#3893)
    **Motivation**
    When using `import` subcommand with a chain.rlp file that doesn't match
    the network, we issue the regular `Parent Block Not Found` error. We are
    able to distinguish this specific case as it will mean that the parent
    block of block number 1 (aka the genesis block) is not found. This PR
    takes advantage of this to issue a specific, more informative warning
    message for this specific case
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    * Add a separate warning message when the imported chain file is not
    compatible with the genesis
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes #3813

--- cmd/ethrex/cli.rs
+++ cmd/ethrex/cli.rs
@@ -410,7 +410,11 @@ pub async fn import_blocks(
             blockchain
                 .add_block(block)
                 .await
-                .inspect_err(|_| warn!("Failed to add block {number} with hash {hash:#x}",))?;
+                .inspect_err(|err| match err {
+                    // Block number 1's parent not found, the chain must not belong to the same network as the genesis file
+                    ChainError::ParentNotFound if number == 1 => warn!("The chain file is not compatible with the genesis file. Are you sure you selected the correct network?"),
+                    _ => warn!("Failed to add block {number} with hash {hash:#x}"),
+                })?;
         }
 
         _ = store
