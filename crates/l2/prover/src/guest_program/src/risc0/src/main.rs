e19613907f6c7b4838422eb066a31367bfb9bb3c
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2025-09-04 18:29:40 -0300

    feat(l1, l2): print zkVM cycles of guest programs (#4273)
    **Description**
    
    Prints zkVM cycles of guest program execution
    
    *SP1 Example*
    
    ```
    2025-09-02T22:50:28.896156Z  INFO ethrex_replay::fetcher: Retrieving execution data for block 23278512 (0 block behind latest)
    2025-09-02T22:50:28.899384Z  WARN ethrex_replay::fetcher: Failed to load cache: No such file or directory (os error 2)
    2025-09-02T22:50:32.833793Z  INFO ethrex_replay::bench: Starting prover program
    2025-09-02T22:50:32.835513Z  WARN sp1_sdk::env: SP1_PROVER environment variable not set, defaulting to 'cpu'
    2025-09-02T22:50:35.038134Z  INFO execute: sp1_core_executor::syscalls::write: ┌╴read_input
    stderr: WARNING: Using insecure random number generator.
    2025-09-02T22:50:35.665546Z  INFO execute: sp1_core_executor::syscalls::write: └╴34,125,436 cycles
    2025-09-02T22:50:35.665570Z  INFO execute: sp1_core_executor::syscalls::write: ┌╴execution
    2025-09-02T22:51:13.041072Z  INFO execute: sp1_core_executor::syscalls::write: └╴1,458,794,351 cycles
    2025-09-02T22:51:13.041100Z  INFO execute: sp1_core_executor::syscalls::write: ┌╴commit_public_inputs
    2025-09-02T22:51:13.042884Z  INFO execute: sp1_core_executor::syscalls::write: └╴26,791 cycles
    2025-09-02T22:51:13.104707Z  INFO execute: sp1_prover: gas: 1687294509
    2025-09-02T22:51:13.107200Z  INFO ethrex_prover_lib::backends::sp1: Successfully executed SP1 program in 38.11s
    2025-09-02T22:51:13.109821Z  INFO ethrex_replay::bench: Total gas from block/s: 23830149
    ```

--- crates/l2/prover/src/guest_program/src/risc0/src/main.rs
+++ crates/l2/prover/src/guest_program/src/risc0/src/main.rs
@@ -2,8 +2,24 @@ use guest_program::{execution::execution_program, input::JSONProgramInput};
 use risc0_zkvm::guest::env;
 
 fn main() {
+    println!("start reading input");
+    let start = env::cycle_count();
     let input: JSONProgramInput = env::read();
-    let output = execution_program(input.0).unwrap();
+    let end = env::cycle_count();
+    println!("end reading input, cycles: {}", end - start);
 
+    println!("start execution");
+    let output = execution_program(input.0).unwrap();
+    let end_exec = env::cycle_count();
+    println!("end execution, cycles: {}", end_exec - end);
+
+    println!("start committing public inputs");
     env::commit_slice(&output.encode());
+    let end_commit = env::cycle_count();
+    println!(
+        "end committing public inputs, cycles: {}",
+        end_commit - end_exec
+    );
+
+    println!("total cycles: {}", end_commit - start);
 }
