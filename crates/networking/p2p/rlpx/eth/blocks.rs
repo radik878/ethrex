35aa069e0d24eb4e697eb718e628aeccb8e17099
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-09-26 10:50:13 -0300

    feat(l1): handle non-canonical headers by hash in `GetBlockHeaders` (#4576)
    **Motivation**
    
    We're currently returning empty header responses when we receive a
    `GetBlockHeaders` request specifying the start with the block's hash,
    and the hash isn't part of our canonical view of the chain.
    
    **Description**
    
    This PR adds support for this case by simply looking for the starting
    block in the DB and returning it. This stays simple enough and, though
    suboptimal, should make the simple case of a peer asking for a sidechain
    to work.
    
    It could be further extended by looking for the block's parent through
    the parent hash, which should allow us to support cases where `reverse`
    is `true`.

--- crates/networking/p2p/rlpx/eth/blocks.rs
+++ crates/networking/p2p/rlpx/eth/blocks.rs
@@ -10,15 +10,24 @@ use ethrex_rlp::{
     error::{RLPDecodeError, RLPEncodeError},
     structs::{Decoder, Encoder},
 };
-use ethrex_storage::Store;
+use ethrex_storage::{Store, error::StoreError};
 use tracing::{error, trace};
 
 pub const HASH_FIRST_BYTE_DECODER: u8 = 160;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum HashOrNumber {
     Hash(BlockHash),
     Number(BlockNumber),
+}
+
+impl core::fmt::Display for HashOrNumber {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            HashOrNumber::Hash(hash) => write!(f, "{hash:#x}"),
+            HashOrNumber::Number(number) => write!(f, "{number}"),
+        }
+    }
 }
 
 impl RLPEncode for HashOrNumber {
@@ -84,28 +93,30 @@ impl GetBlockHeaders {
             reverse,
         }
     }
+
     pub async fn fetch_headers(&self, storage: &Store) -> Vec<BlockHeader> {
+        // According to the spec, we don't need to service non-canonical headers,
+        // but geth does, and it helps in reorg scenarios, so we handle that case.
         let start_block = match self.startblock {
-            // Check we have the given block hash and fetch its number
+            // Try to fetch the block number from the hash
+            // Otherwise keep the hash
             HashOrNumber::Hash(block_hash) => {
-                // TODO(#1073)
-                // Research what we should do when an error is found in a P2P request.
                 if let Ok(Some(block_number)) = storage.get_block_number(block_hash).await {
-                    block_number
+                    HashOrNumber::Number(block_number)
                 } else {
-                    trace!("Could not fetch block number for hash {block_hash}");
-                    return vec![];
+                    self.startblock
                 }
             }
             // Don't check if the block number is available
             // because if it it's not, the loop below will
             // break early and return an empty vec.
-            HashOrNumber::Number(block_num) => block_num,
+            HashOrNumber::Number(_) => self.startblock,
         };
 
         let mut headers = vec![];
 
-        let mut current_block = start_block as i64;
+        let mut current_block = start_block;
+
         let block_skip = if self.reverse {
             -((self.skip + 1) as i64)
         } else {
@@ -119,23 +130,45 @@ impl GetBlockHeaders {
         };
 
         for _ in 0..limit {
-            match storage.get_block_header(current_block as u64) {
-                Ok(Some(block_header)) => {
-                    headers.push(block_header);
-                    current_block += block_skip
-                }
-                Ok(None) => {
+            let block_header_opt = match get_block_header(storage, current_block) {
+                Ok(block_header) => block_header,
+                Err(err) => {
+                    error!(%err, block_ref=%current_block, "Error accessing DB while building header response for peer");
                     break;
                 }
-                // TODO(#1073)
-                // Research what we should do when an error is found in a P2P request.
-                Err(err) => {
-                    error!("Error accessing DB while building header response for peer: {err}");
-                    return vec![];
+            };
+            let Some(block_header) = block_header_opt else {
+                trace!(block_ref=%current_block, "Block header not found");
+                break;
+            };
+            headers.push(block_header);
+
+            // Update to next block to fetch
+            match current_block {
+                // We don't support fetching multiple headers by hash, unless it's
+                // part of the canonical chain, so we break here.
+                // TODO: we could support fetching by hash in descending order,
+                // by fetching the parent of each block.
+                HashOrNumber::Hash(_) => break,
+                HashOrNumber::Number(number) => {
+                    let Ok(new_number) = (number as i64 + block_skip).try_into() else {
+                        break;
+                    };
+                    current_block = HashOrNumber::Number(new_number)
                 }
             }
         }
         headers
+    }
+}
+
+fn get_block_header(
+    storage: &Store,
+    block_ref: HashOrNumber,
+) -> Result<Option<BlockHeader>, StoreError> {
+    match block_ref {
+        HashOrNumber::Hash(block_hash) => storage.get_block_header_by_hash(block_hash),
+        HashOrNumber::Number(block_number) => storage.get_block_header(block_number),
     }
 }
 
@@ -148,7 +181,7 @@ impl RLPxMessage for GetBlockHeaders {
         let reverse = self.reverse as u8;
         Encoder::new(&mut encoded_data)
             .encode_field(&self.id)
-            .encode_field(&(self.startblock.clone(), limit, skip, reverse))
+            .encode_field(&(self.startblock, limit, skip, reverse))
             .finish();
         let msg_data = snappy_compress(encoded_data)?;
         buf.put_slice(&msg_data);
