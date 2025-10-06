c2efc2460e6d3941efdb07b5a60ea92da2436ac8
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-10-06 12:33:31 -0300

    test(l1): full Sync re org tests (#4673)
    **Motivation**
    We want to test different reorg situations.
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    We want to test a reorg where we have 3 nodes. One with the base chain,
    one with a side chain and a third one where we want to full sync the
    base chain. As per this issue
    https://github.com/lambdaclass/ethrex/issues/4617
    Adds a test for that scenario, this test is currently flaky, since 50%
    of the times it will choose the peer with the base chain, working fine,
    but the other 50% it will choose the peer with the side chain, where it
    currently fails.
    Since there is always a 50% chance of failing, we run the test 30 times
    to make sure the probability of failure is small enough.
    Other tests are also added:
    `test_reorg_back_to_base`: Tests a node that syncs to a side chain and
    wants to sync back to the base chain
    `test_reorg_back_to_base_with_common_ancestor`: Same test as before but
    the base chain has blocks
    `test_storage_slots_reorg`: test using a simple contract that sets a
    storage slot as received by calldata
    `no_reorgs_full_sync_smoke_test`: Tests a normal fullsync without reorgs
    
    Also fixes the reorgs CI check, which is being flaky.
    
    ---------
    
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
    Co-authored-by: Camila Di Ielsi <camila.diielsi@lambdaclass.com>
    Co-authored-by: cdiielsi <49721261+cdiielsi@users.noreply.github.com>

--- tooling/reorgs/src/main.rs
+++ tooling/reorgs/src/main.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 use ethrex::{cli::Options, initializers::init_tracing};
+use ethrex_common::U256;
 use ethrex_l2_rpc::signer::{LocalSigner, Signer};
 use tokio::sync::Mutex;
 use tracing::{error, info, warn};
@@ -31,7 +32,17 @@ async fn main() {
     info!("Starting test run");
     info!("");
 
+    run_test(&cmd_path, no_reorgs_full_sync_smoke_test).await;
+    run_test(&cmd_path, test_reorg_back_to_base).await;
+    // This test is flaky 50% of the time, check that it runs correctly 30 times in a row
+    // TODO(#4775): make it deterministic
+    for _ in 0..30 {
+        run_test(&cmd_path, test_chain_split).await;
+    }
     run_test(&cmd_path, test_one_block_reorg_and_back).await;
+    run_test(&cmd_path, test_reorg_back_to_base_with_common_ancestor).await;
+    run_test(&cmd_path, test_storage_slots_reorg).await;
+
     run_test(&cmd_path, test_many_blocks_reorg).await;
 }
 
@@ -52,7 +63,10 @@ where
     let start = std::time::Instant::now();
 
     info!(test=%test_name, "Running test");
-    let simulator = Arc::new(Mutex::new(Simulator::new(cmd_path.to_path_buf())));
+    let simulator = Arc::new(Mutex::new(Simulator::new(
+        cmd_path.to_path_buf(),
+        test_name.to_string(),
+    )));
 
     // Run in another task to clean up properly on panic
     let result = tokio::spawn(test_fn(simulator.clone())).await;
@@ -72,6 +86,85 @@ where
     }
     // Add a blank line after each test for readability
     info!("");
+}
+
+async fn no_reorgs_full_sync_smoke_test(simulator: Arc<Mutex<Simulator>>) {
+    let mut simulator = simulator.lock().await;
+
+    // Start two ethrex nodes
+    let node0 = simulator.start_node().await;
+    let node1 = simulator.start_node().await;
+
+    // Create a chain and extend it with a few empty blocks
+    let base_chain = node0.extend_chain(simulator.get_base_chain(), 10).await;
+
+    // Try to fully sync node1 (which is a peer of node0)
+    node1.update_forkchoice(&base_chain).await;
+}
+
+async fn test_reorg_back_to_base(simulator: Arc<Mutex<Simulator>>) {
+    let mut simulator = simulator.lock().await;
+
+    // Start two ethrex nodes
+    let node0 = simulator.start_node().await; // Test_node
+    let node1 = simulator.start_node().await;
+
+    let base_chain = simulator.get_base_chain();
+    let side_chain = base_chain.fork();
+    // Create a chain and extend it with a few empty blocks
+    let base_chain = node1.extend_chain(base_chain, 10).await;
+
+    // Create another chain (a fork of the first one) and extend it with a few empty blocks
+    let _ = node0.extend_chain(side_chain, 10).await;
+
+    // Try to fully sync node0 to base chain
+    node0.update_forkchoice(&base_chain).await;
+}
+
+async fn test_reorg_back_to_base_with_common_ancestor(simulator: Arc<Mutex<Simulator>>) {
+    let mut simulator = simulator.lock().await;
+
+    // Start two ethrex nodes
+    let node0 = simulator.start_node().await; // Test_node
+    let node1 = simulator.start_node().await;
+
+    let base_chain = simulator.get_base_chain();
+    // Create a chain and extend it with a few empty blocks
+    let base_chain = node1.extend_chain(base_chain, 10).await;
+
+    // Update node0 to the base chain
+    node0.update_forkchoice(&base_chain).await;
+
+    let side_chain = base_chain.fork();
+    // Extend the base chain
+    let base_chain = node1.extend_chain(base_chain, 10).await;
+    // Create another chain (a fork of the first one) and extend it with a few empty blocks
+    let _ = node0.extend_chain(side_chain, 10).await;
+
+    // Try to fully sync node0 to base chain
+    node0.update_forkchoice(&base_chain).await;
+}
+
+async fn test_chain_split(simulator: Arc<Mutex<Simulator>>) {
+    let mut simulator = simulator.lock().await;
+
+    // Start three ethrex nodes
+    let node0 = simulator.start_node().await; // Test_node
+    let node1 = simulator.start_node().await;
+    let node2 = simulator.start_node().await;
+
+    let base_chain = simulator.get_base_chain();
+    let side_chain = base_chain.fork();
+    // Create a chain and extend it with a few empty blocks
+    let base_chain = node1.extend_chain(base_chain, 10).await;
+
+    // Create another chain (a fork of the first one) and extend it with a few empty blocks
+    let _ = node2.extend_chain(side_chain, 10).await;
+
+    // Try to fully sync node0 to base chain (which is a peer of node1 and node2)
+    // It will ask peer1 sometimes and peer2 others, it has to work with both
+    // So if one fails, this test will fail 50% of the time
+    node0.update_forkchoice(&base_chain).await;
 }
 
 async fn test_one_block_reorg_and_back(simulator: Arc<Mutex<Simulator>>) {
@@ -213,4 +306,108 @@ async fn test_many_blocks_reorg(simulator: Arc<Mutex<Simulator>>) {
     // Check the transfer has been processed
     let new_balance = node0.get_balance(recipient).await;
     assert_eq!(new_balance, initial_balance + transfer_amount);
+}
+
+async fn test_storage_slots_reorg(simulator: Arc<Mutex<Simulator>>) {
+    let mut simulator = simulator.lock().await;
+    // Initcode for deploying a contract that receives two `bytes32` parameters and sets `storage[param0] = param1`
+    let contract_deploy_bytecode = hex::decode("656020355f35555f526006601af3").unwrap().into();
+    let signer: Signer = LocalSigner::new(
+        "941e103320615d394a55708be13e45994c7d93b932b064dbcb2b511fe3254e2e"
+            .parse()
+            .unwrap(),
+    )
+    .into();
+
+    let slot_key0 = U256::from(42);
+    let slot_value0 = U256::from(1163);
+    let slot_key1 = U256::from(25);
+    let slot_value1 = U256::from(7474);
+
+    let node0 = simulator.start_node().await;
+    let node1 = simulator.start_node().await;
+
+    // Create a chain with a few empty blocks
+    let mut base_chain = simulator.get_base_chain();
+
+    // Send a deploy tx for a contract which receives: `(bytes32 key, bytes32 value)` as parameters
+    let contract_address = node0
+        .send_contract_deploy(&signer, contract_deploy_bytecode)
+        .await;
+
+    for _ in 0..10 {
+        let extended_base_chain = node0.build_payload(base_chain).await;
+        node0.notify_new_payload(&extended_base_chain).await;
+        node0.update_forkchoice(&extended_base_chain).await;
+
+        node1.notify_new_payload(&extended_base_chain).await;
+        node1.update_forkchoice(&extended_base_chain).await;
+        base_chain = extended_base_chain;
+    }
+
+    // Sanity check: storage slots are initially empty
+    let initial_value = node0.get_storage_at(contract_address, slot_key0).await;
+    assert_eq!(initial_value, U256::zero());
+    let initial_value = node0.get_storage_at(contract_address, slot_key1).await;
+    assert_eq!(initial_value, U256::zero());
+
+    // Fork the chain
+    let mut side_chain = base_chain.fork();
+
+    // Create a side chain with multiple blocks only known to node0
+    for _ in 0..10 {
+        side_chain = node0.build_payload(side_chain).await;
+        node0.notify_new_payload(&side_chain).await;
+        node0.update_forkchoice(&side_chain).await;
+    }
+
+    // Advance the base chain with multiple blocks only known to node1
+    for _ in 0..10 {
+        base_chain = node1.build_payload(base_chain).await;
+        node1.notify_new_payload(&base_chain).await;
+        node1.update_forkchoice(&base_chain).await;
+    }
+
+    // Set a storage slot in the contract in node0
+    let calldata0 = [slot_key0.to_big_endian(), slot_value0.to_big_endian()]
+        .concat()
+        .into();
+    node0.send_call(&signer, contract_address, calldata0).await;
+
+    // Set another storage slot in the contract in node1
+    let calldata1 = [slot_key1.to_big_endian(), slot_value1.to_big_endian()]
+        .concat()
+        .into();
+    node1.send_call(&signer, contract_address, calldata1).await;
+
+    // Build a block in the side chain
+    side_chain = node0.build_payload(side_chain).await;
+    node0.notify_new_payload(&side_chain).await;
+    node0.update_forkchoice(&side_chain).await;
+
+    // Build a block in the base chain
+    base_chain = node1.build_payload(base_chain).await;
+    node1.notify_new_payload(&base_chain).await;
+    node1.update_forkchoice(&base_chain).await;
+
+    // Assert the storage slots are as expected in both forks
+    let value_slot0 = node0.get_storage_at(contract_address, slot_key0).await;
+    assert_eq!(value_slot0, slot_value0);
+    let value_slot1 = node0.get_storage_at(contract_address, slot_key1).await;
+    assert_eq!(value_slot1, U256::zero());
+
+    let value_slot0 = node1.get_storage_at(contract_address, slot_key0).await;
+    assert_eq!(value_slot0, U256::zero());
+    let value_slot1 = node1.get_storage_at(contract_address, slot_key1).await;
+    assert_eq!(value_slot1, slot_value1);
+
+    // Reorg the node0 to the base chain
+    node0.notify_new_payload(&base_chain).await;
+    node0.update_forkchoice(&base_chain).await;
+
+    // Check the storage slots are as expected after the reorg
+    let value_slot0 = node0.get_storage_at(contract_address, slot_key0).await;
+    assert_eq!(value_slot0, U256::zero());
+    let value_slot1 = node0.get_storage_at(contract_address, slot_key1).await;
+    assert_eq!(value_slot1, slot_value1);
 }
--- tooling/reorgs/src/simulator.rs
+++ tooling/reorgs/src/simulator.rs
@@ -1,8 +1,16 @@
-use std::{fs::File, io::Read, path::PathBuf, process::Stdio, time::Duration};
+use std::{
+    fs::File,
+    io::Read,
+    path::PathBuf,
+    process::Stdio,
+    sync::atomic::AtomicU16,
+    time::{Duration, SystemTime},
+};
 
 use ethrex::{cli::Options, initializers::get_network};
 use ethrex_common::{
-    Bytes, H160, H256, U256,
+    Address, Bytes, H160, H256, U256,
+    evm::calculate_create_address,
     types::{
         Block, EIP1559Transaction, Genesis, Transaction, TxKind, requests::compute_requests_hash,
     },
@@ -26,6 +34,8 @@ use tracing::{error, info};
 
 pub struct Simulator {
     cmd_path: PathBuf,
+    test_name: String,
+
     base_opts: Options,
     jwt_secret: Bytes,
     genesis_path: PathBuf,
@@ -35,7 +45,7 @@ pub struct Simulator {
 }
 
 impl Simulator {
-    pub fn new(cmd_path: PathBuf) -> Self {
+    pub fn new(cmd_path: PathBuf, test_name: String) -> Self {
         let mut opts = Options::default_l1();
         let jwt_secret = generate_jwt_secret();
         std::fs::write("jwt.hex", hex::encode(&jwt_secret)).unwrap();
@@ -52,6 +62,7 @@ impl Simulator {
         opts.network = Some(Network::GenesisPath(genesis_path.clone()));
         Self {
             cmd_path,
+            test_name,
             base_opts: opts,
             genesis_path,
             jwt_secret,
@@ -69,18 +80,27 @@ impl Simulator {
 
     pub async fn start_node(&mut self) -> Node {
         let n = self.configs.len();
+        let test_name = &self.test_name;
         info!(node = n, "Starting node");
         let mut opts = self.base_opts.clone();
-        opts.http_port = (8545 + n * 2).to_string();
-        opts.authrpc_port = (8545 + n * 2 + 1).to_string();
-        opts.p2p_port = (30303 + n).to_string();
-        opts.discovery_port = (30303 + n).to_string();
-        opts.datadir = format!("data/node{n}").into();
+        opts.datadir = format!("data/{test_name}/node{n}").into();
+
+        opts.http_port = get_next_port().to_string();
+        opts.authrpc_port = get_next_port().to_string();
+
+        // These are one TCP and one UDP
+        let p2p_port = get_next_port();
+        opts.p2p_port = p2p_port.to_string();
+        opts.discovery_port = p2p_port.to_string();
 
         let _ = std::fs::remove_dir_all(&opts.datadir);
         std::fs::create_dir_all(&opts.datadir).expect("Failed to create data directory");
 
-        let logs_file_path = format!("data/node{n}.log");
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let logs_file_path = format!("data/{test_name}/node{n}_{now}.log");
         let logs_file = File::create(&logs_file_path).expect("Failed to create logs file");
 
         let cancel = CancellationToken::new();
@@ -274,6 +294,15 @@ impl Node {
         chain
     }
 
+    pub async fn extend_chain(&self, mut chain: Chain, num_blocks: usize) -> Chain {
+        for _ in 0..num_blocks {
+            chain = self.build_payload(chain).await;
+            self.notify_new_payload(&chain).await;
+        }
+        self.update_forkchoice(&chain).await;
+        chain
+    }
+
     pub async fn notify_new_payload(&self, chain: &Chain) {
         let head = chain.blocks.last().unwrap();
         let execution_payload = ExecutionPayload::from_block(head.clone());
@@ -333,14 +362,96 @@ impl Node {
             .unwrap();
     }
 
+    pub async fn send_call(&self, signer: &Signer, contract: H160, data: Bytes) {
+        info!(node = self.index, sender=%signer.address(), %contract, "Sending contract call");
+        let chain_id = self
+            .rpc_client
+            .get_chain_id()
+            .await
+            .unwrap()
+            .try_into()
+            .unwrap();
+        let sender_address = signer.address();
+        let nonce = self
+            .rpc_client
+            .get_nonce(sender_address, BlockIdentifier::Tag(BlockTag::Latest))
+            .await
+            .unwrap();
+        let tx = EIP1559Transaction {
+            chain_id,
+            nonce,
+            max_priority_fee_per_gas: 0,
+            max_fee_per_gas: 1_000_000_000,
+            gas_limit: 50_000,
+            to: TxKind::Call(contract),
+            data,
+            ..Default::default()
+        };
+        let mut tx = Transaction::EIP1559Transaction(tx);
+        tx.sign_inplace(signer).await.unwrap();
+        let encoded_tx = tx.encode_canonical_to_vec();
+        self.rpc_client
+            .send_raw_transaction(&encoded_tx)
+            .await
+            .unwrap();
+    }
+
+    pub async fn send_contract_deploy(
+        &self,
+        signer: &Signer,
+        contract_deploy_bytecode: Bytes,
+    ) -> Address {
+        info!(node = self.index, sender=%signer.address(), "Deploying contract");
+        let chain_id = self
+            .rpc_client
+            .get_chain_id()
+            .await
+            .unwrap()
+            .try_into()
+            .unwrap();
+        let sender_address = signer.address();
+        let nonce = self
+            .rpc_client
+            .get_nonce(sender_address, BlockIdentifier::Tag(BlockTag::Latest))
+            .await
+            .unwrap();
+        let tx = EIP1559Transaction {
+            chain_id,
+            nonce,
+            max_priority_fee_per_gas: 0,
+            max_fee_per_gas: 1_000_000_000,
+            gas_limit: 100_000,
+            to: TxKind::Create,
+            data: contract_deploy_bytecode,
+            ..Default::default()
+        };
+        let mut tx = Transaction::EIP1559Transaction(tx);
+        tx.sign_inplace(signer).await.unwrap();
+        let encoded_tx = tx.encode_canonical_to_vec();
+        self.rpc_client
+            .send_raw_transaction(&encoded_tx)
+            .await
+            .unwrap();
+
+        calculate_create_address(sender_address, nonce)
+    }
+
     pub async fn get_balance(&self, address: H160) -> U256 {
         self.rpc_client
             .get_balance(address, Default::default())
             .await
             .unwrap()
     }
+
+    pub async fn get_storage_at(&self, address: H160, key: U256) -> U256 {
+        self.rpc_client
+            .get_storage_at(address, key, Default::default())
+            .await
+            .unwrap()
+    }
 }
 
+#[derive(Debug)]
 pub struct Chain {
     block_hashes: Vec<H256>,
     blocks: Vec<Block>,
@@ -430,4 +541,9 @@ async fn wait_until_synced(engine_client: &EngineClient, fork_choice_state: Fork
         }
         tokio::time::sleep(Duration::from_millis(100)).await;
     }
+}
+
+fn get_next_port() -> u16 {
+    static NEXT_PORT: AtomicU16 = AtomicU16::new(8560);
+    NEXT_PORT.fetch_add(1, std::sync::atomic::Ordering::Relaxed)
 }
