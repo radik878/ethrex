7a4b41b9e230893780c449b60a1db0364730ca58
Mario Rugiero <mrugiero@gmail.com>
2025-10-24 10:33:50 -0300

    perf(l1, l2): use fxhash for diff layers (#5032)
    This PR replaces the hash function used for the diff layers hash map to
    improve performance
    
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes #issue_number
    
    ---------
    
    Co-authored-by: Javier Chatruc <jrchatruc@gmail.com>

--- crates/storage/trie_db/layering.rs
+++ crates/storage/trie_db/layering.rs
@@ -1,12 +1,13 @@
 use ethrex_common::H256;
 use ethrex_rlp::decode::RLPDecode;
-use std::{collections::HashMap, sync::Arc, sync::RwLock};
+use rustc_hash::FxHashMap;
+use std::{sync::Arc, sync::RwLock};
 
 use ethrex_trie::{EMPTY_TRIE_HASH, Nibbles, Node, TrieDB, TrieError};
 
 #[derive(Debug)]
 struct TrieLayer {
-    nodes: HashMap<Vec<u8>, Vec<u8>>,
+    nodes: FxHashMap<Vec<u8>, Vec<u8>>,
     parent: H256,
     id: usize,
 }
@@ -16,7 +17,7 @@ pub struct TrieLayerCache {
     /// Monotonically increasing ID for layers, starting at 1.
     /// TODO: this implementation panics on overflow
     last_id: usize,
-    layers: HashMap<H256, TrieLayer>,
+    layers: FxHashMap<H256, TrieLayer>,
 }
 
 impl TrieLayerCache {
@@ -74,7 +75,7 @@ impl TrieLayerCache {
             .or_insert_with(|| {
                 self.last_id += 1;
                 TrieLayer {
-                    nodes: HashMap::new(),
+                    nodes: FxHashMap::default(),
                     parent,
                     id: self.last_id,
                 }
