1f422e931e0c7900bbb5d905f145da92e1881def
fedacking <francisco.gauna@lambdaclass.com>
2025-10-31 16:33:07 -0300

    fix(l1): fix illegal instructions in certain ARM processors (#5143)
    **Motivation**
    
    During ARM testing, ethrex would crash on certain ARM CPUs. The
    backtrace of the program was in the blake2b precompile in
    `crates/common/crypto/blake2f/aarch64.rs`. The specific instruction that
    crashed was the [XAR
    instruction](https://developer.arm.com/documentation/ddi0602/2022-03/SIMD-FP-Instructions/XAR--Exclusive-OR-and-Rotate-),
    which the documentation states that "This instruction is implemented
    only when FEAT_SHA3 is implemented."
    
    **Description**
    
    - Added check to `BLAKE2_FUNC` lazy evaluator to check for the `sha3`
    feature in the user's cpu at runtime.
    
    **Notes on testing**
    
    On an ARM CPU without the feature `sha3` the commit
    `fba993c9b337ab2a89664f0f39260b40e25ac4c8` includes a failing unit test.
    After `21fb29e439261de5c9f275f54a9de218759daa2f` the test should work.

--- crates/common/crypto/blake2f/mod.rs
+++ crates/common/crypto/blake2f/mod.rs
@@ -10,7 +10,9 @@ type Blake2Func = fn(usize, &mut [u64; 8], &[u64; 16], &[u64; 2], bool);
 
 static BLAKE2_FUNC: LazyLock<Blake2Func> = LazyLock::new(|| {
     #[cfg(target_arch = "aarch64")]
-    if std::arch::is_aarch64_feature_detected!("neon") {
+    if std::arch::is_aarch64_feature_detected!("neon")
+        && std::arch::is_aarch64_feature_detected!("sha3")
+    {
         return self::aarch64::blake2b_f;
     }
 
@@ -24,4 +26,35 @@ static BLAKE2_FUNC: LazyLock<Blake2Func> = LazyLock::new(|| {
 
 pub fn blake2b_f(rounds: usize, h: &mut [u64; 8], m: &[u64; 16], t: &[u64; 2], f: bool) {
     BLAKE2_FUNC(rounds, h, m, t, f)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn blake2b_smoke() {
+        let mut h = [1, 2, 3, 4, 5, 6, 7, 8];
+        blake2b_f(
+            12,
+            &mut h,
+            &[
+                101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
+            ],
+            &[1000, 1001],
+            true,
+        );
+        assert_eq!(
+            h,
+            [
+                16719151077261791083,
+                2946084527549390899,
+                18258373236029374890,
+                15305391278487550604,
+                16233503039257535911,
+                17654926667207417465,
+                12194914407095793501,
+                13409096818966589674
+            ]
+        );
+    }
 }
